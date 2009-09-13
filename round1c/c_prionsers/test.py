5e7531dce56d884f1c8004ff46ca2e4c4354ca16
Alecs King <alecsk@gmail.com>
2009-09-13 21:00:26 +0800

    clean up -- shorter code, faster speed

--- round1c/c_prionsers/test.py
+++ round1c/c_prionsers/test.py
@@ -15,16 +15,11 @@ def readints(): return map(lambda x: int(x), raw_input().strip().split())
 def get():
     p, q = readints()
     id = [0] + readints() + [p + 1] # add two ends
-    seg = [id[x] - id[x - 1] - 1 for x in xrange(1, len(id))]
-    @memo
-    def s(i, j):
-        if i == j: return seg[i]
-        return seg[i] + s(i + 1, j) + 1
     @memo
     def f(i, j): # best of [i..j]
         if i == j: return 0
-        return min(f(i, k) + f(k + 1, j) + s(i, j) - 1 for k in xrange(i, j))
-    return f(0, len(seg) - 1)
+        return min(f(i, k) + f(k + 1, j) + id[j+1] - id[i] - 2 for k in xrange(i, j))
+    return f(0, q)
         
 n = input()
 for x in xrange(1, n + 1):
